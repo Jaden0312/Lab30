bbf3c7298021ad77bbc8bebec87cd4417a0307b4
RyanKhPark <53807537+RyanKhPark@users.noreply.github.com>
2023-11-20 10:18:51 +0900

    data.js add, app.js arrange

---
+++ src/Api/data.js
@@ -0,0 +1,32 @@
+import top_2 from '../top_2.jpg';
+import top_4 from '../top_4.jpg';
+import knit_1 from '../knit_1.jpg';
+
+const data = [
+    {
+        id: 1,
+        name: "코튼 베이직 반팔티",
+        category: "top",
+        image: top_2,
+        new_price: "₩22,900",
+        old_price: "₩29,900",
+    },
+    {
+        id: 5,
+        name: "아카이브 스카시 맨투맨",
+        category: "top",
+        image: top_4,
+        new_price: "₩44,900",
+        old_price: "₩51,900",
+    },
+    {
+        id: 7,
+        name: "마티느 오버핏 니트",
+        category: "knit",
+        image: knit_1,
+        new_price: "₩59,900",
+        old_price: "₩66,900",
+    },
+]
+
+export default data;
--- src/App.js
+++ src/App.js
@@ -1,77 +1,68 @@
 import React from "react";
 import './App.css';
-import Items from './Components/Items/Items';
-import top_2 from "./top_2.jpg";
-import top_4 from "./top_4.jpg";
-import knit_1 from "./knit_1.jpg";
+
+//data를 다른 파일에서 불러옴
+import data from './Api/data.js';
+
+import Items from './Components/Items/Items.js'
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = [
-      {
-        id: 1,
-        name: "코튼 베이직 반팔티",
-        category: "top",
-        image: top_2,
-        new_price: "₩22,900",
-        old_price: "₩29,900",
-      },
-      {
-        id: 5,
-        name: "아카이브 스카시 맨투맨",
-        category: "top",
-        image: top_4,
-        new_price: "₩44,900",
-        old_price: "₩51,900",
-      },
-      {
-        id: 7,
-        name: "마티느 오버핏 니트",
-        category: "knit",
-        image: knit_1,
-        new_price: "₩59,900",
-        old_price: "₩66,900",
-      },
-    ]
-    console.log(this.state);
+    this.state = {
+      productData: [],
+    }
   }
 
-  
-  render() {
-  const handleChange = (event) => {
-    const filteredProduct = this.state.filter((item) => {
-      return item.name.includes(event.target.value);
+  componentDidMount() {
+    // data.js 에서 온 데이터를 this.state 의 productData 의 [] state를 productData 의 []에 data가 들어간 state 로 setState 함.
+
+    this.setState(() => {
+      return { productData: data };
     });
 
-    this.setState((state) => {
-      return {
-        name: filteredProduct,
-      }
-    })
+
   }
-  return (
-    <>
-      <div className="container">
-        <div className="search-container">
-          <input id="searchInput" type="text" placeholder="Write product Name" onChange={handleChange} />
-        </div>
-        <div className="product">
-          <h1>Products</h1>
-          <hr />
-          <div className="product-item">
-            {this.state.map((item) => {
-              return <Items key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} />
-            })}
-            {console.log(this.state)}
+
+
+  render() {
+    console.log(this.state.productData)
+    // 콘솔창을 보면 처음 render() 이 실행 되면 productData 는 [] 인데 바로 다음에 뜨는 콘솔로그는 productData 가 [{}, {}, {}] 인 모양을 보면 아, render() 실행 후 componentDidMount() 실행 되어 state가 바뀌었고, state 가 바뀌었으니 render() 이 다시 돌았구나 알수있음
+    const handleChange = (event) => {
+      const filteredProduct = this.state.productData.filter((item) => {
+        return item.name.includes(event.target.value);
+      });
+
+      // 콘솔창에 보면 filteredProduct 는 필터가 잘 되는 것으로 보임!
+      console.log(filteredProduct)
+
+      this.setState((state) => {
+        return {
+          name: filteredProduct,
+        }
+      })
+    }
+    return (
+      <>
+        <div className="container">
+          <div className="search-container">
+            <input id="searchInput" type="text" placeholder="Write product Name" onChange={handleChange} />
+          </div>
+          <div className="product">
+            <h1>Products</h1>
+            <hr />
+            <div className="product-item">
+              {this.state.productData.map((product) => {
+                return <Items key={product.id} image={product.image} name={product.name} new_price={product.new_price} />
+              })}
+            </div>
           </div>
         </div>
-      </div>
 
 
-    </>
-  );
-}
+      </>
+    );
+  }
 }
 
 
